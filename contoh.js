807364067b09c55b842d8b67b742cc3b219702ff
bestall1 <90543945+bestall1@users.noreply.github.com>
2021-10-02 18:57:02 +0700

    Update contoh.js

--- contoh.js
+++ contoh.js
@@ -9,6 +9,6 @@ if( get_param("lang") != "0" ){
 	var b = ['aff_short_key', 'aliexpress', 'deep', 'link'];
 	var c = ['.aliexpress'];
 	var mykey 	= a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6];
-	// console.log('https://s.click.'+b[1]+'.com/'+b[2]+'_'+b[3]+'.htm?'+b[0]+'='+mykey+'&dl_target_url=https://'+c[0]+'.com/item/'+get_param("prod")+'.html');
+	// console.log('https://s.click.'+b[1]+'.com/'+b[2]+'_'+b[3]+'.htm?'+b[0]+'='+mykey+'&dl_target_url=https://'+get_param("lang")+c[0]+'.com/item/'+get_param("prod")+'.html');
 	window.location.href = 'https://s.click.'+b[1]+'.com/'+b[2]+'_'+b[3]+'.htm?'+b[0]+'='+mykey+'&dl_target_url=https://'+get_param("lang")+c[0]+'.com/item/'+get_param("prod")+'.html';
 }
